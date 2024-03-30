53b422a5d4293a13d7b890ab26f4aa2597277e00
EliannRaven <nicolas.habisreutinger@gmail.com>
2024-03-30 11:41:13 +0100

    Link urls + first class base view list

--- Gorae/Articles/models.py
+++ Gorae/Articles/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class MerchType(models.Model):
+    name = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.name
+
 class Artist(models.Model):
     name = models.CharField(max_length = 200)
+    image = models.ImageField(upload_to='images/artist', blank = True, null = True)
 
     def __str__(self):
         return self.name
     
 class Group(models.Model):
     name = models.CharField(max_length = 200)
+    image = models.ImageField(upload_to='images/group', blank = True, null = True)
     artist = models.ManyToManyField(Artist, blank = True)
 
     def __str__(self):
         return self.name
+    
+class Merch(models.model):
+    name = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.name
+
--- Gorae/Articles/views.py
+++ Gorae/Articles/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+# Import models
+from .models import MerchType
+from .models import Artist
+from .models import Group
+from .models import Merch
+
+# Create views
+# Listview
+class MerchTypeList(ListView):
+    model = MerchType
+    context_object_name = 'MerchTypeList'
+    template_name = 'Articles/MerchTypeList.html'
+
+class ArtistList(ListView):
+    model = Artist
+    context_object_name = 'ArtistList'
+    template_name = 'Articles/ArtistList.html'
+
+class GroupList(ListView):
+    model = Group
+    context_object_name = 'GroupList'
+    template_name = 'Articles/GroupList.html'
+
+class MerchList(ListView):
+    model = Merch
+    context_object_name = 'MerchList'
+    template_name = 'Articles/MerchList.html'
