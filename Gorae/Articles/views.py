b153a4073f6698831bc379cc216f087a38316c98
EliannRaven <nicolas.habisreutinger@gmail.com>
2024-03-30 11:58:02 +0100

    Create, Update, Delete class base view

--- Gorae/Articles/views.py
+++ Gorae/Articles/views.py
@@ -13,20 +13,93 @@ from .models import Merch
 # Listview
 class MerchTypeList(ListView):
     model = MerchType
-    context_object_name = 'MerchTypeList'
+    context_object_name = 'MerchType'
     template_name = 'Articles/MerchTypeList.html'
 
 class ArtistList(ListView):
     model = Artist
-    context_object_name = 'ArtistList'
+    context_object_name = 'Artist'
     template_name = 'Articles/ArtistList.html'
 
 class GroupList(ListView):
     model = Group
-    context_object_name = 'GroupList'
+    context_object_name = 'Group'
     template_name = 'Articles/GroupList.html'
 
 class MerchList(ListView):
     model = Merch
-    context_object_name = 'MerchList'
+    context_object_name = 'Merch'
     template_name = 'Articles/MerchList.html'
+
+# Createview
+
+class MerchTypeCreate(CreateView):
+    model = MerchType
+    context_object_name = 'MerchType'
+    fields = '__all__'
+    template_name = 'Articles/MerchTypeForm.html'
+
+class ArtistCreate(CreateView):
+    model = Artist
+    context_object_name = 'Artist'
+    fields = '__all__'
+    template_name = 'Articles/ArtistForm.html'
+
+class GroupCreate(CreateView):
+    model = Group
+    context_object_name = 'Group'
+    fields = '__all__'
+    template_name = 'Articles/GroupForm.html'
+
+class MerchCreate(CreateView):
+    model = Merch
+    context_object_name = 'Merch'
+    fields = '__all__'
+    template_name = 'Articles/MerchForm.html'
+
+#Update view
+    
+class MerchTypeUpdate(UpdateView):
+    model = MerchType
+    fields = '__all__'
+    template_name = 'Articles/MerchTypeForm.html'
+
+class ArtistUpdate(UpdateView):
+    model = Artist
+    fields = '__all__'
+    template_name = 'Articles/ArtistForm.html'
+
+class GroupUpdate(UpdateView):
+    model = Group
+    fields = '__all__'
+    template_name = 'Articles/GroupForm.html'
+
+class MerchUpdate(UpdateView):
+    model = Merch
+    fields = '__all__'
+    template_name = 'Articles/MerchForm.html'
+
+#Delete view
+class MerchTypeDelete(DeleteView):
+    model = MerchType
+    context_object_name = 'MerchType'
+    template_name = 'Articles/MerchTypeDelete.html'
+    success_url = reverse_lazy('')
+
+class ArtistDelete(DeleteView):
+    model = Artist
+    context_object_name = 'Artist'
+    template_name = 'Articles/ArtistDelete.html'
+    success_url = reverse_lazy('')
+
+class GroupDelete(DeleteView):
+    model = Group
+    context_object_name = 'Group'
+    template_name = 'Articles/GroupDelete.html'
+    success_url = reverse_lazy('')
+
+class MerchDelete(DeleteView):
+    model = Merch
+    context_object_name = 'Merch'
+    template_name = 'Articles/MerchDelete.html'
+    success_url = reverse_lazy('')
